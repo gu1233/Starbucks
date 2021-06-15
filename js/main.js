dd28cf753d1a72c5bbe03cfe2a428b192f425100
gu1233 <gde102@naver.com>
2021-06-15 22:33:43 +0900

    visual section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//검색
 const searchEl = document.querySelector('.search')
 const searchInputEl = searchEl.querySelector('input')
 
@@ -15,7 +16,7 @@ searchInputEl.addEventListener('blur', function(){
   searchInputEl.setAttribute('placeholder', '');
 });
 
-
+//배지
 const badgeEl = document.querySelector('header .badges');
 
 window.addEventListener('scroll', _.throttle(function () {
@@ -34,3 +35,15 @@ window.addEventListener('scroll', _.throttle(function () {
     });
   }
 }, 3000));
+//_.throttle(함수, 시간)
+
+
+//순차적으로 이미지 보이기
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+  // gsap.to(요소, 지속시간, 옵션);
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7, //0.7 1.4 2.1 2.8
+    opacity: 1
+  });
+});
