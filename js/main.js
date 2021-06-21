e56c4b84a3df2184072f2a10638642746b69059c
gu1233 <gde102@naver.com>
2021-06-21 16:36:42 +0900

    공지사항 swiper

--- js/main.js
+++ js/main.js
@@ -47,3 +47,11 @@ fadeEls.forEach(function (fadeEl, index) {
     opacity: 1
   });
 });
+
+//공지사항 swiper
+//new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical', 
+  autoplay: true, 
+  loop: true
+});
