c0fc95e26cf4682ff7ad80d3749e5c69f1b187c5
gu1233 <gde102@naver.com>
2021-06-23 16:19:22 +0900

    스크롤 위치 계산 애니메이션

--- js/main.js
+++ js/main.js
@@ -73,6 +73,12 @@ new Swiper('.promotion .swiper-container', {
     nextEl: '.promotion .swiper-next'
   }
 });
+new Swiper('.awards .swiper-container', {
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5
+});
 
 
 const promotionEl = document.querySelector('.promotion');
@@ -112,3 +118,16 @@ function floatingObject(selector, delay, size) {
 floatingObject('.floating1', 1, 15);
 floatingObject('.floating2', .5, 15);
 floatingObject('.floating3', 1.5, 20);
+
+
+// scrollMagic
+const spyEl = document.querySelectorAll('section.scroll-spy');
+spyEl.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({
+      triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+      triggerHook: .8 
+    })
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller());
+});
